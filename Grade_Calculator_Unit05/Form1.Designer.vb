da7ab85c939ccbaccf30dc0ae91d565d34531f31
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-16 08:43:02 -0500

    Another commit

--- Grade_Calculator_Unit05/Form1.Designer.vb
+++ Grade_Calculator_Unit05/Form1.Designer.vb
@@ -25,24 +25,25 @@ Partial Class frmMain
         Me.txtNumAssignments = New System.Windows.Forms.TextBox()
         Me.btnEnterGrades = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.lblFinalGrade = New System.Windows.Forms.Label()
         Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblFinalGrade = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'txtNumAssignments
         '
-        Me.txtNumAssignments.Location = New System.Drawing.Point(12, 33)
+        Me.txtNumAssignments.Location = New System.Drawing.Point(12, 28)
         Me.txtNumAssignments.Name = "txtNumAssignments"
         Me.txtNumAssignments.Size = New System.Drawing.Size(196, 20)
-        Me.txtNumAssignments.TabIndex = 0
+        Me.txtNumAssignments.TabIndex = 1
         Me.txtNumAssignments.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'btnEnterGrades
         '
-        Me.btnEnterGrades.Location = New System.Drawing.Point(12, 59)
+        Me.btnEnterGrades.Location = New System.Drawing.Point(12, 52)
         Me.btnEnterGrades.Name = "btnEnterGrades"
         Me.btnEnterGrades.Size = New System.Drawing.Size(196, 40)
-        Me.btnEnterGrades.TabIndex = 1
+        Me.btnEnterGrades.TabIndex = 2
         Me.btnEnterGrades.Text = "&Enter Grades"
         Me.btnEnterGrades.UseVisualStyleBackColor = True
         '
@@ -50,42 +51,53 @@ Partial Class frmMain
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(51, 17)
+        Me.Label1.Location = New System.Drawing.Point(51, 11)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(118, 13)
-        Me.Label1.TabIndex = 2
+        Me.Label1.TabIndex = 0
         Me.Label1.Text = "Enter # of assignments:"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(13, 242)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(196, 33)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
         'lblFinalGrade
         '
         Me.lblFinalGrade.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblFinalGrade.Enabled = False
-        Me.lblFinalGrade.Location = New System.Drawing.Point(12, 102)
+        Me.lblFinalGrade.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFinalGrade.Location = New System.Drawing.Point(12, 96)
         Me.lblFinalGrade.Name = "lblFinalGrade"
-        Me.lblFinalGrade.Size = New System.Drawing.Size(196, 109)
-        Me.lblFinalGrade.TabIndex = 3
-        Me.lblFinalGrade.Text = " "
+        Me.lblFinalGrade.Size = New System.Drawing.Size(196, 105)
+        Me.lblFinalGrade.TabIndex = 4
+        Me.lblFinalGrade.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'btnExit
+        'btnClear
         '
-        Me.btnExit.Location = New System.Drawing.Point(12, 214)
-        Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(196, 33)
-        Me.btnExit.TabIndex = 4
-        Me.btnExit.Text = "E&xit"
-        Me.btnExit.UseVisualStyleBackColor = True
+        Me.btnClear.Location = New System.Drawing.Point(13, 205)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(196, 33)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(220, 249)
-        Me.Controls.Add(Me.btnExit)
+        Me.ClientSize = New System.Drawing.Size(220, 286)
+        Me.Controls.Add(Me.btnClear)
         Me.Controls.Add(Me.lblFinalGrade)
+        Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnEnterGrades)
         Me.Controls.Add(Me.txtNumAssignments)
         Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Grade Calculator"
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -95,6 +107,7 @@ Partial Class frmMain
     Friend WithEvents txtNumAssignments As TextBox
     Friend WithEvents btnEnterGrades As Button
     Friend WithEvents Label1 As Label
-    Friend WithEvents lblFinalGrade As Label
     Friend WithEvents btnExit As Button
+    Friend WithEvents lblFinalGrade As Label
+    Friend WithEvents btnClear As Button
 End Class
--- Grade_Calculator_Unit05/Form1.vb
+++ Grade_Calculator_Unit05/Form1.vb
@@ -13,22 +13,57 @@
 
         'Declares variables:
         Dim intNumAssignments As Integer
+        Dim dblSingleAssignmentGrade As Double
+        Dim dblTotalAssignmentGrade As Double
+        Dim intCounter As Integer = 0
 
         'Gets number of assignments:
         Integer.TryParse(txtNumAssignments.Text, intNumAssignments)
 
+        'prompts user to enter grade for each assignment in an inputbox, 
+        'adds all grades together:
+        Do While (intCounter < intNumAssignments)
+            dblSingleAssignmentGrade = InputBox("Individual Grades",
+                                                "Enter individual grade:")
+            dblTotalAssignmentGrade = dblTotalAssignmentGrade + dblSingleAssignmentGrade
+            intCounter = intCounter + 1
+        Loop
 
+        'Caculates average grade:
+        dblTotalAssignmentGrade = dblTotalAssignmentGrade / intNumAssignments
+        dblTotalAssignmentGrade = dblTotalAssignmentGrade.ToString("N1")
 
+        'Process letter grade, prints result in label control:
+        Select Case dblTotalAssignmentGrade
+            Case 90 To 100
+                lblFinalGrade.Text = "The final letter grade is: A"
+            Case 80 To 89.9
+                lblFinalGrade.Text = "The final letter grade is: B"
+            Case 70 To 70.9
+                lblFinalGrade.Text = "The final letter grade is: C"
+            Case 60 To 60.9
+                lblFinalGrade.Text = "The final letter grade is: D"
+            Case < 60
+                lblFinalGrade.Text = "You failed this class with a grade of: F"
+        End Select
 
     End Sub
 
     Private Sub txtNumAssignments_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNumAssignments.KeyPress
 
-        'Allows only numbers and the backspace key:
+        'Validation: Allows only numbers and the backspace key:
         If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
                 e.KeyChar <> ControlChars.Back Then
             e.Handled = True
         End If
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        'Clears the text boxes and labels for another entry:
+        txtNumAssignments.Text = ""
+        lblFinalGrade.Text = ""
 
     End Sub
 End Class
